391b9906006de649d5a88e38085e161a8f7ea188
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-02-23 08:46:33 +0300

    add basic functions for the LinkedList class

--- main.js
+++ main.js
@@ -4,3 +4,85 @@ class Node {
     this.nextNode = nextNode;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    if (this.length != 0) {
+      let currentNode = this.first;
+      while (currentNode.nextNode != null) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = new Node(value);
+      this.last = currentNode.nextNode;
+    } else {
+      this.first = new Node(value);
+      this.last = this.first;
+    }
+    this.length += 1;
+  }
+
+  prepend(value) {
+    this.first = new Node(value, this.first);
+    this.length += 1;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  head() {
+    if (this.length != 0) {
+      return this.first.value;
+    } else {
+      return undefined;
+    }
+  }
+
+  tail() {
+    if (this.length != 0) {
+      return this.last.value;
+    } else {
+      return undefined;
+    }
+  }
+
+  at(index) {
+    if (index < 0 || index > this.length - 1) {
+      return undefined;
+    } else {
+      let currentNode = this.first;
+      for (let i = 0; i < index; i++) {
+        currentNode = currentNode.nextNode;
+      }
+      return currentNode.value;
+    }
+  }
+
+  pop() {
+    if (this.length != 0) {
+      let value = this.first.value;
+      this.first = this.first.nextNode;
+      this.length -= 1;
+      if (this.length == 0) {
+        this.last = null;
+      }
+      return value;
+    } else {
+      return undefined;
+    }
+  }
+}
+
+const fruits = new LinkedList();
+
+fruits.append("banana");
+fruits.append("apple");
+fruits.append("pineapple");
+fruits.prepend("orange");
+console.log(fruits);
